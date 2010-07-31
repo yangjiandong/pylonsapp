9fb7711f626e43c1af498cf7a9cc9be280e9f1cc
young.jiandong <young.jiandong@gmail.com>
2010-07-31 17:56:16 +0800

    update ..

--- pylonsapp/controllers/page.py
+++ pylonsapp/controllers/page.py
@@ -6,6 +6,9 @@ from pylons import request, response, session, tmpl_context as c
 #from pylons.controllers.util import abort, redirect_to
 # 推荐采用h
 import pylonsapp.lib.helpers as h
+import pylonsapp.model.meta as meta
+#from pylonsapp.model import *
+from pylonsapp.model import Page
 
 from pylonsapp.lib.base import BaseController, render
 
@@ -13,9 +16,17 @@ log = logging.getLogger(__name__)
 
 class PageController(BaseController):
 
-    def view(self, id):
-        c.title = 'Greetings'
-        c.heading = 'Sample Page'
-        c.content = 'This is page %s'%id
+    def view(self, id=None):
+#        c.title = 'Greetings'
+#        c.heading = 'Sample Page'
+#        c.content = 'This is page %s'%id
+
+        if id is None:
+            h.abort(404)
+
+        page_q = meta.Session.query(Page)
+        c.page = page_q.get(int(id))
+        if c.page is None:
+            h.abort(404)
         return render('/derived/page/view.html')
 
